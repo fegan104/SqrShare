e88829eef12b4bebf8b57ad2a584a5caec7957f1
fegan104 <fegan104@gmail.com>
2024-06-11 22:19:00 -0400

    Added compose tests

--- app/src/androidTest/kotlin/SquareImageLoaderTest.kt
+++ app/src/androidTest/kotlin/SquareImageLoaderTest.kt
@@ -1,18 +1,12 @@
 import android.content.Context
-import android.content.res.AssetManager
 import android.graphics.Bitmap
-import androidx.core.content.FileProvider
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import com.frankegan.sqrshare.SqrBitmapGenerator
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStream
 import kotlin.test.assertTrue
 
 
@@ -23,27 +17,7 @@ class SquareImageLoaderTest {
     fun testLoadFromUri() = runTest {
         // Copy file from assets
         val context: Context = ApplicationProvider.getApplicationContext()
-        val assetManager: AssetManager = InstrumentationRegistry.getInstrumentation().context.assets
-        val inputStream = assetManager.open("testimage.jpg")
-
-        //write to app-specific external files dir
-        val outputDir = InstrumentationRegistry.getInstrumentation().targetContext.getExternalFilesDir(null)
-        val outputFile = File(outputDir, "testimage.jpg")
-        val outputStream: OutputStream = FileOutputStream(outputFile)
-        val buffer = ByteArray(1024)
-        var length: Int
-        while (inputStream.read(buffer).also { length = it } > 0) {
-            outputStream.write(buffer, 0, length)
-        }
-
-        inputStream.close()
-        outputStream.close()
-
-        val imageUri = FileProvider.getUriForFile(
-            InstrumentationRegistry.getInstrumentation().targetContext,
-            InstrumentationRegistry.getInstrumentation().targetContext.packageName + ".fileprovider",
-            outputFile
-        )
+        val imageUri = getUriForFile("testimage.jpg")
 
         // Generate square bitmap
         val bitmap: Bitmap? = SqrBitmapGenerator.generate(context, imageUri)
---
+++ app/src/androidTest/kotlin/TestExtensions.kt
@@ -0,0 +1,31 @@
+import android.content.res.AssetManager
+import android.net.Uri
+import androidx.core.content.FileProvider
+import androidx.test.platform.app.InstrumentationRegistry
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+
+fun getUriForFile(fileName: String): Uri {
+    val assetManager: AssetManager = InstrumentationRegistry.getInstrumentation().context.assets
+    val inputStream = assetManager.open(fileName)
+
+    //write to app-specific external files dir
+    val outputDir = InstrumentationRegistry.getInstrumentation().targetContext.getExternalFilesDir(null)
+    val outputFile = File(outputDir, fileName)
+    val outputStream: OutputStream = FileOutputStream(outputFile)
+    val buffer = ByteArray(1024)
+    var length: Int
+    while (inputStream.read(buffer).also { length = it } > 0) {
+        outputStream.write(buffer, 0, length)
+    }
+
+    inputStream.close()
+    outputStream.close()
+
+    return FileProvider.getUriForFile(
+        InstrumentationRegistry.getInstrumentation().targetContext,
+        InstrumentationRegistry.getInstrumentation().targetContext.packageName + ".fileprovider",
+        outputFile
+    )
+}
